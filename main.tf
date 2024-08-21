9474e1a74929f947893bccf0533601708e481c5c
davidthowes <david.howes@hashicorp.com>
2024-08-21 16:29:54 -0400

    Add conditions

--- main.tf
+++ main.tf
@@ -46,4 +46,17 @@ resource "aws_instance" "web" {
   tags = {
     Name = "HelloWorld"
   }
+
+  lifecycle {
+    # The AMI ID must refer to an AMI that contains an operating system
+    # for the `x86_64` architecture.
+    precondition {
+      condition     = data.aws_ami.aws_linux.architecture == "x86_64"
+      error_message = "The selected AMI must be for the x86_64 architecture."
+    }
+    
+    postcondition {
+      condition     = self.instance_type == "t3.micro"
+      error_message = "EC2 instance must be a t3.micro so that I can give this demo."
+    }
 }
