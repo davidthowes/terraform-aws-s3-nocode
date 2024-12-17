edc763a96e88bb66a3d39326cbd5eeb9ebb8a2bc
davidthowes <david.howes@hashicorp.com>
2024-12-17 13:04:48 -0500

    Update main.tf

--- main.tf
+++ main.tf
@@ -2,7 +2,7 @@ terraform {
   required_providers {
     aws = {
       source  = "hashicorp/aws"
-      version = "~> 4.0"
+      version = "~> 5.0"
     }
   }
 }
@@ -13,52 +13,12 @@ provider "aws" {
 }
 
 # Configure the S3 bucket
-resource "aws_s3_bucket" "b" {
+resource "aws_s3_bucket" "my_bucket" {
   bucket        = var.bucket_name
 
   tags = {
     Name        = "My bucket"
     Environment = "Dev"
     Terraform   = "True"
-  }
-}
-
-data "aws_ami" "aws_linux" {
-  most_recent = true
-
-  filter {
-    name   = "architecture"
-    values = [var.architecture]
-  }
-
-  filter {
-    name   = "virtualization-type"
-    values = ["hvm"]
-  }
-
-  owners = ["amazon"] 
-}
-
-resource "aws_instance" "web" {
-  ami           = data.aws_ami.aws_linux.id
-  instance_type = "t3.micro"
-
-  tags = {
-    Name = "HelloWorld"
-  }
-
-  lifecycle {
-    # The AMI ID must refer to an AMI that contains an operating system
-    # for the `x86_64` architecture.
-    precondition {
-      condition     = data.aws_ami.aws_linux.architecture == "x86_64"
-      error_message = "The selected AMI must be for the x86_64 architecture."
-    }
-
-    # The EC2 instance must be allocated a public DNS hostname.
-    postcondition {
-      condition     = self.public_dns != ""
-      error_message = "EC2 instance must be in a VPC that has public DNS hostnames enabled."
-    }
   }
 }
